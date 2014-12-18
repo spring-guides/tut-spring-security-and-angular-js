c202ec85e39365ee69a25ada0ba58f53a2c813b6
Dave Syer <dsyer@pivotal.io>
2014-12-18 10:07:44 +0000

    Upgrde Spring Boot

--- certs/src/main/java/sample/tomcat/SslApplication.java
+++ certs/src/main/java/sample/tomcat/SslApplication.java
@@ -16,24 +16,12 @@
 
 package sample.tomcat;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,54 +43,6 @@ public class SslApplication {
 	@RequestMapping("/hello")
 	public String helloWorld() {
 		return "hello";
-	}
-
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-		tomcat.addConnectorCustomizers(new TomcatConnectorCustomizer() {
-			@Override
-			public void customize(Connector connector) {
-				SslApplication.this.customize(connector);
-			}
-		});
-		return tomcat;
-	}
-
-	private Connector customize(Connector connector) {
-		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-		try {
-			File keystore = getKeyStoreFile();
-			File truststore = keystore;
-			connector.setScheme("https");
-			connector.setSecure(true);
-			Integer port = this.server.getPort();
-			connector.setPort(port == null ? 8443 : port);
-			protocol.setClientAuth("true");
-			protocol.setSSLEnabled(true);
-			protocol.setKeystoreFile(keystore.getAbsolutePath());
-			protocol.setKeystorePass("password");
-			protocol.setTruststoreFile(truststore.getAbsolutePath());
-			protocol.setTruststorePass("password");
-			protocol.setKeyAlias("servercert");
-			return connector;
-		}
-		catch (IOException ex) {
-			throw new IllegalStateException("cant access keystore: [" + "keystore"
-					+ "] or truststore: [" + "keystore" + "]", ex);
-		}
-	}
-
-	private File getKeyStoreFile() throws IOException {
-		ClassPathResource resource = new ClassPathResource("server.jks");
-		try {
-			return resource.getFile();
-		}
-		catch (Exception ex) {
-			File temp = File.createTempFile("server.jks", ".tmp");
-			FileCopyUtils.copy(resource.getInputStream(), new FileOutputStream(temp));
-			return temp;
-		}
 	}
 
 	public static void main(String[] args) throws Exception {

888b614ab9b989e72f7866d3c27214e51bb554b2
Dave Syer <dsyer@pivotal.io>
2015-01-02 11:58:26 +0000

    Add OAuth2 sample

---
+++ pairs/oauth2/authserver/src/main/java/demo/AuthserverApplication.java
@@ -0,0 +1,107 @@
+package demo;
+
+import java.security.KeyPair;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+@SessionAttributes("authorizationRequest")
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserverApplication.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
+
+	@Configuration
+	@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+		
+		@Autowired
+		private AuthenticationManager authenticationManager;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
+					.anyRequest().authenticated();
+		}
+		
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.parentAuthenticationManager(authenticationManager);
+		}
+	}
+
+	@Configuration
+	@EnableAuthorizationServer
+	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+		@Autowired
+		private AuthenticationManager authenticationManager;
+
+		@Bean
+		public JwtAccessTokenConverter jwtAccessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			KeyPair keyPair = new KeyStoreKeyFactory(
+					new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+					.getKeyPair("test");
+			converter.setKeyPair(keyPair);
+			return converter;
+		}
+
+		@Override
+		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+			clients.inMemory()
+					.withClient("acme")
+					.secret("acmesecret")
+					.authorizedGrantTypes("authorization_code", "refresh_token",
+							"password").scopes("openid");
+		}
+
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+				throws Exception {
+			endpoints.authenticationManager(authenticationManager).accessTokenConverter(
+					jwtAccessTokenConverter());
+		}
+
+		@Override
+		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+				throws Exception {
+			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
+					"isAuthenticated()");
+		}
+
+	}
+}
---
+++ pairs/oauth2/ui/src/main/java/demo/UiApplication.java
@@ -0,0 +1,89 @@
+package demo;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@RestController
+@EnableZuulProxy
+@EnableOAuth2Sso
+public class UiApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Configuration
+	protected static class SecurityConfiguration extends OAuth2SsoConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http.logout().and().antMatcher("/**").authorizeRequests()
+					.antMatchers("/**/*.html", "/", "/login").permitAll().anyRequest()
+					.authenticated().and().csrf()
+					.csrfTokenRepository(csrfTokenRepository()).and()
+					.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+		}
+
+		private Filter csrfHeaderFilter() {
+			return new OncePerRequestFilter() {
+				@Override
+				protected void doFilterInternal(HttpServletRequest request,
+						HttpServletResponse response, FilterChain filterChain)
+						throws ServletException, IOException {
+					CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+							.getName());
+					if (csrf != null) {
+						Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+						String token = csrf.getToken();
+						if (cookie == null || token != null
+								&& !token.equals(cookie.getValue())) {
+							cookie = new Cookie("XSRF-TOKEN", token);
+							cookie.setPath("/");
+							response.addCookie(cookie);
+						}
+					}
+					filterChain.doFilter(request, response);
+				}
+			};
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
+
+}

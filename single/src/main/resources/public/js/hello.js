9f0fae8a80124c40d029da1b28f1be61fa59b395
Dave Syer <dsyer@pivotal.io>
2015-01-02 07:12:07 +0000

    Add single projecty (merges UI and resources)

---
+++ single/src/main/resources/public/js/hello.js
@@ -0,0 +1,79 @@
+angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider) {
+
+	$routeProvider.when('/', {
+		templateUrl : 'home.html',
+		controller : 'home'
+	}).when('/login', {
+		templateUrl : 'login.html',
+		controller : 'navigation'
+	}).otherwise('/');
+
+}).controller('navigation',
+
+function($rootScope, $scope, $http, $location, $route) {
+
+	$scope.tab = function(route) {
+		return $route.current && route === $route.current.controller;
+	};
+
+	var authenticate = function(callback) {
+
+		$http.get('user').success(function(data) {
+			if (data.name) {
+				$rootScope.authenticated = true;
+			} else {
+				$rootScope.authenticated = false;
+			}
+			callback && callback();
+		}).error(function() {
+			$rootScope.authenticated = false;
+			callback && callback();
+		});
+
+	}
+
+	authenticate();
+
+	$scope.credentials = {};
+	$scope.login = function() {
+		$http.post('login', $.param($scope.credentials), {
+			headers : {
+				"content-type" : "application/x-www-form-urlencoded"
+			}
+		}).success(function(data) {
+			authenticate(function() {
+				if ($rootScope.authenticated) {
+					console.log("Login succeeded")
+					$location.path("/");
+					$scope.error = false;
+					$rootScope.authenticated = true;
+				} else {
+					console.log("Login failed with redirect")
+					$location.path("/login");
+					$scope.error = true;
+					$rootScope.authenticated = false;
+				}
+			});
+		}).error(function(data) {
+			console.log("Login failed")
+			$location.path("/login");
+			$scope.error = true;
+			$rootScope.authenticated = false;
+		})
+	};
+
+	$scope.logout = function() {
+		$http.post('logout', {}).success(function() {
+			$rootScope.authenticated = false;
+			$location.path("/");
+		}).error(function(data) {
+			console.log("Logout failed")
+			$rootScope.authenticated = false;
+		});
+	}
+
+}).controller('home', function($scope, $http) {
+	$http.get('/resource/').success(function(data) {
+		$scope.greeting = data;
+	})
+});

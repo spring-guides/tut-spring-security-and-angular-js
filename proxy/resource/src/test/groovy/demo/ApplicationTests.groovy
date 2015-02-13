2303a0e075fc7b395602efc525f46eb5242cfa02
Dave Syer <dsyer@pivotal.io>
2015-02-13 16:24:00 +0000

    Fix test that broke when we changed auth entry point

--- proxy/resource/src/test/groovy/demo/ApplicationTests.groovy
+++ proxy/resource/src/test/groovy/demo/ApplicationTests.groovy
@@ -29,7 +29,8 @@ public class ApplicationTests {
 	@Test
 	public void resourceProtected() {
 		ResponseEntity<String> response = template.getForEntity("http://localhost:${port}/", String.class)
-		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode())
+		// N.B. better if it was UNAUTHORIZED but that means we have to add a custom authentication entry point
+		assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode())
 	}
 
 }

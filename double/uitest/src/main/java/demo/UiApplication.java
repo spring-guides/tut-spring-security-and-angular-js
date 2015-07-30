ca0a1adea7fb50b334b959735fd3b5effe663cb6
Shannon Pence <spence@craftcruises.com>
2015-07-30 01:22:01 -0600

    Adding the simplest version to replicate the redirect

---
+++ double/uitest/src/main/java/demo/UiApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+
+}

3fa76bfad25ce83a3787b00a40dd8f46653bcc71
Dave Syer <dsyer@pivotal.io>
2016-08-02 11:26:58 -0700

    Update text to match actual idioms in Spring Boot 1.4
    The CORS stuff changed in Spring 4.3 and Spring Security 4.1
    so we needed a quick refresh.

--- spring-session/resource/src/main/java/demo/ResourceApplication.java
+++ spring-session/resource/src/main/java/demo/ResourceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsUtils;
 
 @SpringBootApplication
 @RestController
@@ -19,7 +18,7 @@ public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().requestMatchers(CorsUtils::isCorsRequest).permitAll().anyRequest().authenticated();
+		http.cors().and().authorizeRequests().anyRequest().authenticated();
 	}
 
 	@RequestMapping("/")

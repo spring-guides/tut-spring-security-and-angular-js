543a34f5f4fdc8fdb442c2264284e566fb1ae479
Antoine <antoine.c.larose@gmail.com>
2017-01-20 02:01:20 -0500

    Unecessary @EnableRedisHttpSession
    Unecessary @EnableRedisHttpSession in UiApplication.java?

--- double/ui/src/main/java/demo/UiApplication.java
+++ double/ui/src/main/java/demo/UiApplication.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
 public class UiApplication {
 
 	@RequestMapping("/user")

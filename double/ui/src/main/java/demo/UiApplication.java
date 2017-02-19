629cd1a02f13668dd5d3f48d625bbf6232baf02c
Dave Syer <dsyer@pivotal.io>
2017-02-19 12:01:48 +0000

    Remove unnecessary annotation
    Fixes gh-137

--- double/ui/src/main/java/demo/UiApplication.java
+++ double/ui/src/main/java/demo/UiApplication.java
@@ -11,13 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
 public class UiApplication {
 
 	@RequestMapping("/user")

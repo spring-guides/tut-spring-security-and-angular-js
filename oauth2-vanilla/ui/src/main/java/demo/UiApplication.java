9bfaeb029d35b665e63615b9ceee06d5fb9528d8
Dave Syer <dsyer@pivotal.io>
2017-02-21 13:52:37 +0000

    Add OAuth2RestTemplate bean so tokens can be refreshed
    Fixes gh-140

--- oauth2-vanilla/ui/src/main/java/demo/UiApplication.java
+++ oauth2-vanilla/ui/src/main/java/demo/UiApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
@@ -15,6 +19,12 @@ public class UiApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
+	}
+
+	@Bean
+	protected OAuth2RestTemplate OAuth2RestTemplate(
+			OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
 	}
 
 	@Override

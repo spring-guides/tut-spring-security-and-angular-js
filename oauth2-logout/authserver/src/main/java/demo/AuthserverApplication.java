796cec4fb4f2f90f3b269f8491296de6cf01be5d
Dave Syer <dsyer@pivotal.io>
2017-08-22 13:05:47 +0100

    Switch to session invalidation on authserver

--- oauth2-logout/authserver/src/main/java/demo/AuthserverApplication.java
+++ oauth2-logout/authserver/src/main/java/demo/AuthserverApplication.java
@@ -3,6 +3,10 @@ package demo;
 import java.security.KeyPair;
 import java.security.Principal;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,16 +26,15 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @Controller
@@ -69,28 +71,10 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 			http
 				.formLogin().loginPage("/login").permitAll()
 			.and()
-				.cors().configurationSource(configurationSource())
+				.requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
 			.and()
-				.requestMatchers().antMatchers("/logout", "/login", "/oauth/authorize", "/oauth/confirm_access")
-			.and()
-				.authorizeRequests().anyRequest().authenticated()
-			.and()
-				.csrf()
-					.ignoringAntMatchers("/logout/**")
-					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-	// @formatter:on
-		}
-
-		private CorsConfigurationSource configurationSource() {
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			CorsConfiguration config = new CorsConfiguration();
-			config.addAllowedOrigin("*");
-			config.setAllowCredentials(true);
-			config.addAllowedHeader("X-Requested-With");
-			config.addAllowedHeader("Content-Type");
-			config.addAllowedMethod(HttpMethod.POST);
-			source.registerCorsConfiguration("/logout", config);
-			return source;
+				.authorizeRequests().anyRequest().authenticated();
+			// @formatter:on
 		}
 
 		@Override
@@ -101,8 +85,8 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
 	@Configuration
 	@EnableAuthorizationServer
-	protected static class OAuth2AuthorizationConfig extends
-			AuthorizationServerConfigurerAdapter {
+	protected static class OAuth2AuthorizationConfig
+			extends AuthorizationServerConfigurerAdapter {
 
 		@Autowired
 		private AuthenticationManager authenticationManager;
@@ -112,35 +96,51 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 			KeyPair keyPair = new KeyStoreKeyFactory(
 					new ClassPathResource("keystore.jks"), "foobar".toCharArray())
-					.getKeyPair("test");
+							.getKeyPair("test");
 			converter.setKeyPair(keyPair);
 			return converter;
 		}
 
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory()
-					.withClient("acme")
-					.secret("acmesecret")
-					.autoApprove(true)
+			clients.inMemory().withClient("acme").secret("acmesecret")
 					.authorizedGrantTypes("authorization_code", "refresh_token",
-							"password").scopes("openid");
+							"password")
+					.autoApprove(true)
+					.scopes("openid");
 		}
 
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
 				throws Exception {
-			endpoints.authenticationManager(authenticationManager).accessTokenConverter(
-					jwtAccessTokenConverter());
+			endpoints.authenticationManager(authenticationManager)
+					.accessTokenConverter(jwtAccessTokenConverter());
+			endpoints.addInterceptor(new HandlerInterceptorAdapter() {
+				@Override
+				public void postHandle(HttpServletRequest request,
+						HttpServletResponse response, Object handler,
+						ModelAndView modelAndView) throws Exception {
+					if (modelAndView != null
+							&& modelAndView.getView() instanceof RedirectView) {
+						RedirectView redirect = (RedirectView) modelAndView.getView();
+						String url = redirect.getUrl();
+						if (url.contains("code=") || url.contains("error=")) {
+							HttpSession session = request.getSession(false);
+							if (session != null) {
+								session.invalidate();
+							}
+						}
+					}
+				}
+			});
 		}
 
 		@Override
 		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
 				throws Exception {
-			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
-					"isAuthenticated()");
+			oauthServer.tokenKeyAccess("permitAll()")
+					.checkTokenAccess("isAuthenticated()");
 		}
 
 	}
-
 }

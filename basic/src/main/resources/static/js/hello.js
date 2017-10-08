1efaa3fae6521797f5e4a29a2f16a4d994ad1f0c
Dave Syer <dsyer@pivotal.io>
2017-10-08 13:53:03 +0200

    Translate "basic" section and sample app to Angular
    I decided to ditch wro4j. May revisit that later, given the
    number of dependencies you have to add. Also removed the tests
    for now, so we could put those back later (but they weren't
    part of this section anyway).
    
    See gh-162

--- basic/src/main/resources/static/js/hello.js
+++ basic/src/main/resources/static/js/hello.js
@@ -1,6 +1,20 @@
-angular.module('hello', []).controller('home', function($http) {
-	var self = this;
-	$http.get('resource/').then(function(response) {
-		self.greeting = response.data;
-	})
+var AppComponent = ng.core.Component({
+    selector : 'hello',
+    template : '<p>The ID is {{greeting.id}}</p><p>The content is {{greeting.content}}</p>'
+}).Class({
+    constructor : [ng.http.Http, function(http) {
+        var self = this;
+        self.greeting = {id:'', content:''};
+        http.get("/resource").map(response => response.json()).subscribe(data => self.greeting = data);
+    }]
+});
+
+var AppModule = ng.core.NgModule({
+    imports: [ng.platformBrowser.BrowserModule, ng.http.HttpModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+  }).Class({constructor : function(){}})
+
+document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
 });

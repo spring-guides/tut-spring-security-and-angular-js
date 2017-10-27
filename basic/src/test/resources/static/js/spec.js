1a46d35f39b59628b8436507ca5ef9b37265b107
Dave Syer <dsyer@pivotal.io>
2017-10-27 14:59:02 +0100

    Partial fix and comment out testing section

--- basic/src/test/resources/static/js/spec.js
+++ basic/src/test/resources/static/js/spec.js
@@ -1,24 +1,18 @@
+var TestBed = ng.core.testing.TestBed;
+var async = ng.core.testing.async;
+
 describe("App", function() {
 
-	beforeEach(module('hello'));
-	var $httpBackend, $controller;
-	beforeEach(inject(function($injector) {
-		$httpBackend = $injector.get('$httpBackend');
-		$controller = $injector.get('$controller');
-	}));
-	afterEach(function() {
-		$httpBackend.verifyNoOutstandingExpectation();
-		$httpBackend.verifyNoOutstandingRequest();
-	});
-
-	it("says Hello Test when controller loads", function() {
-		$httpBackend.expectGET('resource/').respond(200, {
-			id : 4321,
-			content : 'Hello Test'
-		});
-		var controller = $controller('home');
-		$httpBackend.flush();
-		expect(controller.greeting.content).toEqual('Hello Test');
-	});
-
+  beforeEach(async(() =>{
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 })

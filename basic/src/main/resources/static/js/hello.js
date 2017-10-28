a939f26695a96b95457fbf34f3e9a99e58b0a3ed
Dave Syer <dsyer@pivotal.io>
2017-10-28 06:30:53 +0100

    Add a basic test back to basic sample
    Command line mvn jasmine:bdd and jasmine:test both work.

--- basic/src/main/resources/static/js/hello.js
+++ basic/src/main/resources/static/js/hello.js
@@ -5,16 +5,14 @@ var AppComponent = ng.core.Component({
     constructor : [ng.http.Http, function(http) {
         var self = this;
         self.greeting = {id:'', content:''};
-        http.get("/resource").subscribe(response => self.greeting =response.json());
+        http.get("/resource").subscribe(function(response) {
+          self.greeting =response.json()
+        });
     }]
 });
 
 var AppModule = ng.core.NgModule({
-    imports: [ng.platformBrowser.BrowserModule, ng.http.HttpModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
-  }).Class({constructor : function(){}})
-
-document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
-});
+  imports: [ng.platformBrowser.BrowserModule, ng.http.HttpModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+}).Class({constructor : function(){}})
--- basic/src/test/resources/static/js/spec.js
+++ basic/src/test/resources/static/js/spec.js
@@ -1,18 +1,42 @@
 var TestBed = ng.core.testing.TestBed;
-var async = ng.core.testing.async;
+var inject = ng.core.testing.inject;
+var ResponseOptions = ng.http.ResponseOptions;
+var Response = ng.http.Response;
 
-describe("App", function() {
+ng.core.testing.getTestBed().initTestEnvironment(
+    ng.platformBrowserDynamic.testing.BrowserDynamicTestingModule,
+    ng.platformBrowserDynamic.testing.platformBrowserDynamicTesting()
+  );
 
-  beforeEach(async(() =>{
+describe("AppComponent", function() {
+
+  beforeEach(function() {
     TestBed.configureTestingModule({
+      imports: [ng.http.HttpModule],
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: ng.http.XHRBackend, useClass: ng.http.testing.MockBackend }
+      ]
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+  });
+
+  it('should create the app', function() {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should download greeting', inject([ng.http.XHRBackend], function(backend) {
+    backend.connections.subscribe(function(connection) {
+      connection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify({id:'ABC',content:'Hello World'})
+      })));
+    });
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.greeting.content).toEqual('Hello World');
   }));
-})
+
+});

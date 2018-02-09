a464ac386dcf3230513522c9f8acb12323d0128b
Dave Syer <dsyer@pivotal.io>
2018-02-09 14:04:53 +0000

    Ensure /logout is included in security config

--- oauth2/authserver/src/main/java/demo/AuthserverApplication.java
+++ oauth2/authserver/src/main/java/demo/AuthserverApplication.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +27,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -64,7 +68,7 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 			http
 				.formLogin().loginPage("/login").permitAll()
 			.and()
-				.requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+				.requestMatchers().antMatchers("/login", "/logout", "/oauth/authorize", "/oauth/confirm_access")
 			.and()
 				.authorizeRequests().anyRequest().authenticated();
 			// @formatter:on
@@ -74,12 +78,13 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 			auth.parentAuthenticationManager(authenticationManager);
 		}
+
 	}
 
 	@Configuration
 	@EnableAuthorizationServer
-	protected static class OAuth2AuthorizationConfig extends
-			AuthorizationServerConfigurerAdapter {
+	protected static class OAuth2AuthorizationConfig
+			extends AuthorizationServerConfigurerAdapter {
 
 		@Autowired
 		private AuthenticationManager authenticationManager;
@@ -89,32 +94,31 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
 			KeyPair keyPair = new KeyStoreKeyFactory(
 					new ClassPathResource("keystore.jks"), "foobar".toCharArray())
-					.getKeyPair("test");
+							.getKeyPair("test");
 			converter.setKeyPair(keyPair);
 			return converter;
 		}
 
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory()
-					.withClient("acme")
-					.secret("acmesecret")
+			clients.inMemory().withClient("acme").secret("acmesecret")
 					.authorizedGrantTypes("authorization_code", "refresh_token",
-							"password").scopes("openid");
+							"password")
+					.scopes("openid");
 		}
 
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
 				throws Exception {
-			endpoints.authenticationManager(authenticationManager).accessTokenConverter(
-					jwtAccessTokenConverter());
+			endpoints.authenticationManager(authenticationManager)
+					.accessTokenConverter(jwtAccessTokenConverter());
 		}
 
 		@Override
 		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
 				throws Exception {
-			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
-					"isAuthenticated()");
+			oauthServer.tokenKeyAccess("permitAll()")
+					.checkTokenAccess("isAuthenticated()");
 		}
 
 	}

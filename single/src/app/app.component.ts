d4ba1c4165fd0ba0a938fb602cbcd61afd6eb5f2
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2018-03-16 12:29:39 +0100

    fixing authenticating user on page reload ({} is resolved to truthy)

--- single/src/app/app.component.ts
+++ single/src/app/app.component.ts
@@ -11,7 +11,7 @@ import 'rxjs/add/operator/finally';
 })
 export class AppComponent {
   constructor(private app: AppService, private http: HttpClient, private router: Router) {
-      this.app.authenticate({}, undefined);
+      this.app.authenticate(undefined, undefined);
     }
     logout() {
       this.http.post('logout', {}).finally(() => {

c00ea27d1f4117f41c619eaa9454a5045e24b376
liuyocean <57547267+liuyocean@users.noreply.github.com>
2020-09-18 16:40:21 -0700

    Update write.component.ts
    Keep the endpoint the same with the post on line 16.

--- double/admin/src/app/write.component.ts
+++ double/admin/src/app/write.component.ts
@@ -9,7 +9,7 @@ export class WriteComponent {
   greeting = {};
 
   constructor(private http: HttpClient) {
-    this.http.get('http://localhost:9000').subscribe(data => this.greeting = data);
+    this.http.get('/resource').subscribe(data => this.greeting = data);
   }
 
   update() {

516e689e06f1c61c845c15bd35595d7f2908e04d
Dave Syer <dsyer@vmware.com>
2021-12-20 09:02:37 +0000

    Upgrade node and frontend plugin

--- single/src/test/java/demo/ApplicationTests.java
+++ single/src/test/java/demo/ApplicationTests.java
@@ -1,19 +1,15 @@
 package demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 

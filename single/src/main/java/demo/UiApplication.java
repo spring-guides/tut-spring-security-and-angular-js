0808b3e601ca32e1a3e04f5d240f7756edb133f7
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-01-30 18:28:41 -0700

    Update single Application
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- single/src/main/java/demo/UiApplication.java
+++ single/src/main/java/demo/UiApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,7 +51,9 @@ public class UiApplication {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
             http
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(basic -> basic
+                    .securityContextRepository(new HttpSessionSecurityContextRepository())
+                )
                 .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers("/index.html", "/", "/home", "/login", "/*.js", "/*.css", "/*.ico").permitAll()
                     .anyRequest().authenticated()
